d8beb5bff44fbc70b26c84857491d22dd67701d0
DannyHeyes <63042730+DannyHeyes@users.noreply.github.com>
2021-03-05 15:44:52 +0100

    about section
    JS accordion added to site for extra info about services offered.

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li')
 
-
+// Burger Annimations
 burger.addEventListener('click', () => {
+  //Adding classlist to nav-links
   nav.classList.toggle('active');
 
+//adding the fade keyframe to each individual link with a delay
   navLinks.forEach((link, index) => {
     if(link.style.animation) {
       link.style.animation = '';
@@ -14,5 +16,19 @@ burger.addEventListener('click', () => {
     }
   });
 
+// adding toggle classlist for animation of burger menu
   burger.classList.toggle('toggle');
 });
+
+
+// Accordion Options Annimations
+const button = document.querySelectorAll('button');
+const panel = document.querySelectorAll('.panel');
+const arrow = document.querySelectorAll('.arrow');
+
+for(let i = 0; i < button.length; i++) {
+  button[i].addEventListener('click', () => {
+    panel[i].classList.toggle('panelActive');
+    arrow[i].classList.toggle('arrowActive');
+  });
+}
